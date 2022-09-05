de0a71ccb6d618e89db0d6c639ea7e4d39e60804
AliyaHeula <yakupovaliya@yandex.ru>
2022-09-06 00:46:34 +0300

    AC and +/- buttons are implemeted and labels is shown

--- calculator/ViewController.swift
+++ calculator/ViewController.swift
@@ -9,12 +9,29 @@ import UIKit
 
 class ViewController: UIViewController {
     @IBOutlet var holder: UIView!
+    @IBOutlet weak var result: UILabel!
+    var number = 0
+    @IBAction func digits(_ sender: UIButton) {
+        result.text = result.text! + String(sender.tag)
+        number = Int(result.text!)!
+    }
 
+    @IBAction func acButton(_ sender: Any) {
+        result.text = ""
+        number = 0
+    }
+    
+    @IBAction func negative(_ sender: Any) {
+        if number != 0 {
+            number *= (-1)
+            result.text = String(number)
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
 
 }
 
