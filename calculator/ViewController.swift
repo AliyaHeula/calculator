6e04dc3cc27021281de7d5a6b48137b862b5076f
AliyaHeula <yakupovaliya@yandex.ru>
2022-09-05 23:59:32 +0300

    view is ready

---
+++ calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  calculator
+//
+//  Created by Aliya on 05.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet var holder: UIView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
